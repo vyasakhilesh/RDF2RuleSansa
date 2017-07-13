5858577b4a06be657c5d7828060c98178643203e
Kunal-Jha <kunal94jha@gmail.com>
2017-07-13 20:17:47 +0200

    Improvised path search working and bug fixing

--- src/main/scala/net/sansa_stack/template/spark/graphOps/AllpathsImpovised.scala
+++ src/main/scala/net/sansa_stack/template/spark/graphOps/AllpathsImpovised.scala
@@ -8,37 +8,41 @@ object AllpathsImpovised {
 
   def getPath(graph: Graph[String, String]): VertexRDD[List[Path]] = {
     val initialMsg: List[Path] = List.empty
-    val maxIterations = 5
+    val maxIterations = 2
     val activeDirection = EdgeDirection.Out
-    val pregelGraph = graph.mapVertices((id, label) => (id, label, List.empty[Path]))
+    val pregelGraph = graph.mapVertices((id, label) => (id, List.empty[Path]))
 
     val messages = pregelGraph.pregel[List[Path]](initialMsg, maxIterations, activeDirection)(
-      (vid, oldMessage, newMessage) => (vid, oldMessage._2, newMessage ++ oldMessage._3),
+      (vid, oldMessage, newMessage) => (vid, newMessage ),
       (triplet) => {
-        val receivedPathsSrc = triplet.srcAttr._3
-        val receivedPathsDest = triplet.dstAttr._3
+        val receivedPathsSrc = triplet.srcAttr._2
+        val receivedPathsDest = triplet.dstAttr._2
 
-        if ((receivedPathsSrc.length == 0) && (receivedPathsDest.length == 0)) {
+        if ((receivedPathsSrc.isEmpty) && (receivedPathsDest.isEmpty)) {
           val path = new Path(1, List(triplet.dstAttr._1))
-          val newMsg = path :: triplet.srcAttr._3
-          Iterator((triplet.srcId, newMsg))
-        } else if ((receivedPathsDest.length == 0) && (receivedPathsSrc.length != 0)) {
+          val newMsg = path :: triplet.srcAttr._2
+          Iterator((triplet.dstId, newMsg))
+        } else if ((receivedPathsSrc.length != 0)) {
           for (a <- receivedPathsDest) {
             if (a.path.contains(triplet.dstId)) {
-              triplet.dstId :: a.path
+              a.path :+ triplet.dstId
+              a.length += 1
+            }
+          }
+          Iterator((triplet.dstId, receivedPathsDest))
+        } /*else if ((receivedPathsDest.length != 0) && (receivedPathsSrc.length != 0)) {
+          for (a <- receivedPathsDest) {
+            if (a.path.contains(triplet.dstId)) {
+              for (x <- triplet.dstAttr._2) {
+                a.path ++ x.path
+                a.length += x.length
+
+              }
             }
           }
 
           Iterator((triplet.srcId, receivedPathsDest))
-        } else if ((receivedPathsDest.length != 0) && (receivedPathsSrc.length != 0)) {
-          for (a <- receivedPathsDest) {
-            if (a.path.contains(triplet.dstId)) {
-              a.path ++ triplet.dstAttr._3
-            }
-          }
-
-          Iterator((triplet.srcId, receivedPathsDest))
-        } else {
+        }*/ else {
           Iterator.empty
         }
       },
@@ -46,8 +50,8 @@ object AllpathsImpovised {
 
     val vertexWithType = messages.vertices
     vertexWithType.collect.foreach(println)
-    graph.vertices.innerJoin(vertexWithType)((vid, label, typeList) => (typeList._3))
+    graph.vertices.innerJoin(vertexWithType)((vid, label, typeList) => (typeList._2))
 
   }
 }
-case class Path(length: Int, path: List[VertexId])
+case class Path(var length: Int, var path: List[VertexId])
