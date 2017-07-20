9695ad94775cdfa5f7a7f75427288eff9b7c5c10
vyasakhilesh <achilles4in@gmail.com>
2017-07-20 16:12:54 +0200

    AddedTypeInfoAndUpdatedGraphWithTypeInfo

--- src/main/scala/net/sansa_stack/template/spark/rdf/entityTypeInfo.scala
+++ src/main/scala/net/sansa_stack/template/spark/rdf/entityTypeInfo.scala
@@ -1,6 +1,24 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/* Author: Akhilesh Vyas
+ *
+ */
 
 package net.sansa_stack.template.spark.rdf
-
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
@@ -8,84 +26,117 @@ import org.apache.spark._
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.rdd.RDD
 import org.apache.spark.graphx._
-//AUTHOR:
 import org.apache.log4j._
 
-object GraphOps {
-  def main(args: Array[String]) = {
-    Logger.getLogger("org.apache.spark.SparkContext").setLevel(Level.WARN)
-    val t1 = System.nanoTime
-    val input = "src/main/resources/datset.nt"
-    val conf = new SparkConf().setAppName("GraphXTest").setMaster("local")
-    val sc = new SparkContext(conf)
-   
-    val spark = SparkSession.builder.master("local[*]").config("spark.serializer", "org.apache.spark.serializer.KryoSerializer").appName("GraphX example").getOrCreate()
-    val tripleRDD = spark.sparkContext.textFile(input).map(TripleUtils.parsTriples)
-    
-    val tutleSubjectObject = tripleRDD.map { x => (x.subject, x.`object`) }
-    
-    type VertexId = Long
-    val indexVertexID = (tripleRDD.map(_.subject) union tripleRDD.map(_.`object`)).distinct().zipWithIndex()
-   
-    val vertices: RDD[(VertexId, String)] = indexVertexID.map(f => (f._2, f._1))
-    
-    val tuples = tripleRDD.keyBy(_.subject).join(indexVertexID).map(
-      {
-        case (k, (TripleUtils.Triples(s, p, o), si)) => (o, (si, p))
-      })
-     
-     
-    val edges: RDD[Edge[String]] = tuples.join(indexVertexID).map({ case (k, ((si, p), oi)) => Edge(si, oi, p) })
- 
-    val graph = Graph(vertices, edges)
-   
-    //Taking one predicate of FPC to know entities meanwhile
-    val predlist = List("http://xmlns.com/foaf/0.1/depiction")
-   //val predlist = graph.edges.flatMap(x=>List(x.attr)).collect().distinct.toList
- 
+object  EntityTypeInformation extends App {
   
+
+  Logger.getLogger("org.apache.spark.SparkContext").setLevel(Level.WARN)
   
-  def searchedge(x:String, y:List[String]):Boolean={
-    y.contains(x)
+  val inputDataSet = "src/main/resources/datset.nt"
+  val conf = new SparkConf().setAppName("GraphXTest").setMaster("local")
+  val sc = new SparkContext(conf)
+   
+  val spark = SparkSession.builder.master("local[*]").config("spark.serializer", "org.apache.spark.serializer.KryoSerializer").appName("GraphX example").getOrCreate()
+  val tripleRDD = spark.sparkContext.textFile(inputDataSet).map(TripleUtils.parsTriples)
+  
+  val tutleSubjectObject = tripleRDD.map { x => (x.subject, x.`object`) }
+  
+  type VertexId = Long
+  val indexVertexID = (tripleRDD.map(_.subject) union tripleRDD.map(_.`object`)).distinct().zipWithIndex()
+     
+  val vertices: RDD[(VertexId, String)] = indexVertexID.map(f => (f._2, f._1))
+      
+  val tuples = tripleRDD.keyBy(_.subject).join(indexVertexID).map(
+        {
+          case (k, (TripleUtils.Triples(s, p, o), si)) => (o, (si, p))
+        })
+       
+       
+  val edges: RDD[Edge[String]] = tuples.join(indexVertexID).map({ case (k, ((si, p), oi)) => Edge(si, oi, p) })
+   
+  val graph = Graph(vertices, edges.distinct())
+     
+      //Taking one predicate of FPC to know entities meanwhile
+  val predicateListFromFpc = List("http://data.semanticweb.org/ns/swc/ontology#heldBy")
+   // val predicateListFromFpc = graph.edges.flatMap(x=>List(x.attr)).collect().distinct.toList
+    
+  val t1 = System.nanoTime
+  
+  def searchEdge(edgeArg1:String, edgeArg2:String):Boolean={
+     edgeArg1==edgeArg2
+    }
+  
+  def searchDesEntity(id:Long, iDlist:List[Long], edgeArgument:String, typeArgument:String):Boolean= {
+      ((iDlist.contains(id))&&(edgeArgument==typeArgument))
+     }
+  
+  def searchId(id:Long, idList:List[Long]):Boolean={
+    
+    idList.contains(id)
     
   }
+    
+  for(aPredict <- predicateListFromFpc)
   
-  //val setofentity = graph.triplets.filter(x=> searchedge(x.attr, predlist)).flatMap(x=>List(x.srcAttr, x.dstAttr))
-  val setofentity = graph.triplets.filter(x=> searchedge(x.attr, predlist)).flatMap(x=>List(x.srcAttr))
-  val setofdistictentity = setofentity.distinct().collect().toList
-  val printsetofdistictentity = setofdistictentity.foreach(println)
+  {
   
-  def searchdes(x:String, y:List[String], a:String, b:String):Boolean= {
-    ((y.contains(x))&&(a==b))
+    val setOfEntity = graph.triplets.filter(triple=> searchEdge(triple.attr, aPredict)).flatMap(triple=>List(triple.srcId)).cache()
+    val setOfDistictEntity = setOfEntity.distinct().collect().toList
+    //val printsetofdistictentity = setOfDistictEntity.foreach(println)
+    
+   
+    val setOfTypeList = graph.triplets.filter(triple=>searchDesEntity(triple.srcId, setOfDistictEntity , triple.attr, "http://www.w3.org/1999/02/22-rdf-syntax-ns#type")).map(x=>(x.dstId, 1)).reduceByKey((a,b)=>(a+b)).cache()
+  
+    val support_count = 1
+    var freqSetOfTypeList = setOfTypeList.filter(feqEntity=>feqEntity._2 >= support_count).sortBy(_._2, false)
+   
+    println("")
+   
+    val k:Int = 4
+  
+    if(freqSetOfTypeList.count() > k ){
+    /*List to RDD transformation Take top K FrequentType */
+     freqSetOfTypeList = sc.parallelize(freqSetOfTypeList.take(k)) 
+     }
+    
+    println("---------------------FreqSetOfTypeListWithVertexId-------------------")
+    val freqsetoftypelistRdd =  freqSetOfTypeList.map(x=>(x._1)).foreach(println)
+    val durationforFreqTypeList = (System.nanoTime - t1) / 1e9d
+     println("\nDone--"+"Duration for FreqSetOfTypeList:"+ durationforFreqTypeList)
+  
+    println("\n---------------------FreqSetOfTypeListWithArgument-------------------")
+    
+    val freqsetoftypelistK = freqSetOfTypeList.map(x=>x._1).collect().toList
+    val freqSetOfTypeListArg = graph.triplets.filter(x=> searchId(x.dstId, freqsetoftypelistK)).map(x=>(x.dstAttr)).distinct()
+    freqSetOfTypeListArg.foreach(println)
+ 
+    
+    /* Adding TypeInformation to Real Graph */
+    val t2 = System.nanoTime
+  
+    val edgeArray:Array[Edge[String]] = Array()
+  
+    for(i <- 0 to (freqSetOfTypeList.collect().toList.length - 1))
+   {
+     for(j <- 0 to (setOfDistictEntity.length - 1 ))
+    {
+       edgeArray:+Edge(setOfDistictEntity(j), freqSetOfTypeList.collect().toList(i)._1,"http://www.w3.org/1999/02/22-rdf-syntax-ns#type" )
+    }
    }
   
- 
-  val setoftypelist = graph.triplets.filter(x=>searchdes(x.srcAttr, setofdistictentity , x.attr, "http://www.w3.org/1999/02/22-rdf-syntax-ns#type")).map(x=>(x.dstAttr, 1)).reduceByKey((a,b)=>(a+b))
-  // val printsetoftypelist = setoftypelist.foreach(println)
-   
-   //Remove Types according to thresold
-   val support_count = 20
-   var freqsetoftypelist = setoftypelist.filter(x=>x._2 >= support_count).sortBy(_._2, false)
-   
-  // val printfreqsetoftypelist = freqsetoftypelist.foreach(println)
-   println("")
-   
-   //Count > K remove
-  //println(freqsetoftypelist.count())
-  val k:Int = 4
-  if(freqsetoftypelist.count() > k )
-  {
-    
-     freqsetoftypelist = sc.parallelize(freqsetoftypelist.take(k)) //I am losing RDD here
-  }
-    
-  println("---------------------Freqsetoftypelist-------------------")
-   freqsetoftypelist.map(x=>(x._1)).foreach(println)
-    
-   // To do Task Merge set of distinct entities (setofdistictentity) and predicate (freqsetoftypelist) and add it to original RDD graph.--------------------------------------
+    val edgeRDD: RDD[Edge[String]] = sc.parallelize(edgeArray)
   
-    spark.stop
-    val duration = (System.nanoTime - t1) / 1e9d
-    println("\nDone--"+"Duration:"+duration)
+    val graphUpdated = Graph(vertices, edges.union(edgeRDD).distinct())
+    println("")
+    println("-----New Updated Edges--------------")
+    println(graphUpdated.edges.count())
+    println("-----Old Updated Edges--------------")
+    println(graph.edges.count())
+  
+  
+    val durationforUpdatedGraph = (System.nanoTime - t2) / 1e9d  
+    println("\nDone--"+"Duration for Updating Graph:"+ durationforUpdatedGraph)
   }
+    spark.stop
 }
